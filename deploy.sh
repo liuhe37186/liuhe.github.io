d89e24610fc1e4f9eaef91e35018126786e1d060
liuhe37186 <liuhe37186@163.com>
2020-03-20 19:10:05 +0800

    测试自动部署

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+# !/usr/bin/env sh
 
 # 当发生错误时中止脚本
 set -e
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # cd 到构建输出的目录下 
-# cd dist
+cd dist
 
 # 部署到自定义域域名
 # echo 'www.example.com' > CNAME
@@ -17,7 +17,7 @@ git add -A
 git commit -m '部署测试'
 
 # 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:liuhe37186/liuhe37186.github.io.git master
+git push -f https://github.com/liuhe37186/liuhe37186.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f https://github.com/xjinky/hello-world.git master:gh-pages  # 提交到gh-master分支
--- src/components/view/Template_3.vue
+++ src/components/view/Template_3.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div>temp3</div>
+    <div>----temp3-----</div>
     <div class="temp3">
       <div class="item" v-for="item in items" :key="item.id" @click="itemClick(item)">
         <div class="image"></div>
